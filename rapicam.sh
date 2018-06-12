a81b5d4d45f9f4e15f701f31331f6a66dfbabefd
Pakpoon <jcubuntu@yahoo.com>
2018-06-12 08:38:40 +0700

    rm rapicam

--- rapicam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "[Install raspicam node]
-cd ~/catkin_ws/src
-git clone https://github.com/UbiquityRobotics/raspicam_node.git
-sudo apt-get install -y ros-kinetic-compressed-image-transport ros-kinetic-camera-info-manager
-cd ~/catkin_ws && catkin_make
