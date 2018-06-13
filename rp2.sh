7ab912d767c00c5dcfc8a536a0f23b72a1f394f9
Pakpoon <jcubuntu@yahoo.com>
2018-06-13 09:23:05 +0700

    fixbug

--- rp2.sh
+++ rp2.sh
@@ -1,1 +1,6 @@
+cd $HOME/catkin_ws/src
+git clone https://github.com/UbiquityRobotics/raspicam_node.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_autorace.git
+cd $HOME/catkin_ws && catkin_make
+
 rosrun turtlebot3_bringup create_udev_rules
